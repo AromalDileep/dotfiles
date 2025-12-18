71fdf5c7d98133cf33287153544772bf1541ac8f
Aromal Dileep <aromaldileep96@gmail.com>
2025-12-18 16:57:16 +0530

    Flash Nvim colour changed

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -1,44 +1,73 @@
 return {
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    opts = {
-      modes = {
-        -- standard jump mode
-        search = {
-          enabled = true,
-        },
-
-        -- OPERATOR MODE (this is what you want)
-        char = {
-          enabled = true,
-        },
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    modes = {
+      -- standard jump mode
+      search = {
+        enabled = true,
+      },
+      -- OPERATOR MODE (this is what you want)
+      char = {
+        enabled = true,
       },
     },
-    keys = {
-      -- Jump (normal/visual/operator)
-      {
-        "s",
-        mode = { "n", "x", "o" },
-        function() require("flash").jump() end,
-        desc = "Flash Jump",
-      },
-
-      -- Treesitter jump
-      {
-        "S",
-        mode = { "n", "x", "o" },
-        function() require("flash").treesitter() end,
-        desc = "Flash Treesitter",
-      },
-
-      -- REMOTE OPERATOR (y/d/c at a distance)
-      {
-        "r",
-        mode = "o",
-        function() require("flash").remote() end,
-        desc = "Flash Remote Operator",
+    -- CUSTOMIZE COLORS HERE
+    highlight = {
+      -- backdrop color (dims the rest of the text)
+      backdrop = true,
+      -- highlight groups for the labels
+      groups = {
+        match = "FlashMatch", -- matched text
+        current = "FlashCurrent", -- current match
+        backdrop = "FlashBackdrop", -- backdrop
+        label = "FlashLabel", -- the jump labels
       },
     },
   },
+  keys = {
+    -- Jump (normal/visual/operator)
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function() require("flash").jump() end,
+      desc = "Flash Jump",
+    },
+    -- Treesitter jump
+    {
+      "S",
+      mode = { "n", "x", "o" },
+      function() require("flash").treesitter() end,
+      desc = "Flash Treesitter",
+    },
+    -- REMOTE OPERATOR (y/d/c at a distance)
+    {
+      "r",
+      mode = "o",
+      function() require("flash").remote() end,
+      desc = "Flash Remote Operator",
+    },
+  },
+  -- ADD THIS CONFIG FUNCTION TO SET CUSTOM COLORS
+  config = function(_, opts)
+    require("flash").setup(opts)
+
+    -- Define your custom colors here
+    vim.api.nvim_set_hl(0, "FlashLabel", {
+      fg = "#ff007c", -- bright pink/magenta
+      bg = "#1a1a1a", -- dark background
+      bold = true,
+    })
+
+    vim.api.nvim_set_hl(0, "FlashMatch", {
+      fg = "#61afef", -- blue
+      bg = "NONE",
+    })
+
+    vim.api.nvim_set_hl(0, "FlashCurrent", {
+      fg = "#e5c07b", -- yellow/gold
+      bg = "#3e4451",
+      bold = true,
+    })
+  end,
 }
