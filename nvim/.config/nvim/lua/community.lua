232fb9ad74811b75c99e87e7e6a71f666887ea39
Aromal Dileep <aromaldileep96@gmail.com>
2025-12-18 15:13:37 +0530

    Configure Catppuccin Macchiato for AstroNvim

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -1,12 +1,14 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
+-- This file is imported before the plugins/ folder
 
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+
+  -- Language packs (keep existing behavior)
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+
+  -- Colorscheme: Catppuccin
+  { import = "astrocommunity.colorscheme.catppuccin" },
 }
+
