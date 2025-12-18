387e3d9536e71d28e774e712f9847c401fcbee47
Aromal Dileep <aromaldileep96@gmail.com>
2025-12-18 16:45:05 +0530

    Update nvim plugins configuration
    - Update lazy-lock.json with latest plugin versions
    - Modify flash.lua plugin settings
    - Add new smear_cursor.lua plugin

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -2,25 +2,43 @@ return {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      modes = {
+        -- standard jump mode
+        search = {
+          enabled = true,
+        },
+
+        -- OPERATOR MODE (this is what you want)
+        char = {
+          enabled = true,
+        },
+      },
+    },
     keys = {
+      -- Jump (normal/visual/operator)
       {
         "s",
         mode = { "n", "x", "o" },
-        function()
-          require("flash").jump()
-        end,
+        function() require("flash").jump() end,
         desc = "Flash Jump",
       },
+
+      -- Treesitter jump
       {
         "S",
         mode = { "n", "x", "o" },
-        function()
-          require("flash").treesitter()
-        end,
+        function() require("flash").treesitter() end,
         desc = "Flash Treesitter",
+      },
+
+      -- REMOTE OPERATOR (y/d/c at a distance)
+      {
+        "r",
+        mode = "o",
+        function() require("flash").remote() end,
+        desc = "Flash Remote Operator",
       },
     },
   },
 }
-
