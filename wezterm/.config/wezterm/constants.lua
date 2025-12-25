a60035e458b245e9a25c6cadd2c54971fdd13f38
Aromal Dileep <aromaldileep96@gmail.com>
2025-12-25 23:19:09 +0530

    added new images for wezterm

--- wezterm/.config/wezterm/constants.lua
+++ wezterm/.config/wezterm/constants.lua
@@ -2,8 +2,9 @@ local M = {}
 
 M.bg_blurred_darker = 'assets/bg-blurred-darker.png'
 M.bg_blurred = 'assets/bg-blurred.png'
-
+M.bg_astro = 'assets/fly_upscayl_realesrgan-x4plus_x2.png'
+M.bg_triangle = 'assets/Honkadaloonga.png'
 -- Default background
-M.bg_image = M.bg_blurred_darker
+M.bg_image = M.bg_blurred
 
 return M
