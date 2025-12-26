f549cc032d047cf249beead07c102210f8f08983
Aromal Dileep <aromaldileep96@gmail.com>
2025-12-26 09:38:06 +0530

    new background image changed

--- wezterm/.config/wezterm/constants.lua
+++ wezterm/.config/wezterm/constants.lua
@@ -4,7 +4,13 @@ M.bg_blurred_darker = 'assets/bg-blurred-darker.png'
 M.bg_blurred = 'assets/bg-blurred.png'
 M.bg_astro = 'assets/fly_upscayl_realesrgan-x4plus_x2.png'
 M.bg_triangle = 'assets/Honkadaloonga.png'
+M.bg_city = 'assets/peakpx.jpg'
+M.bg_city2 = 'assets/city_2.jpg'
+M.color1 = 'assets/color1.jpg'
+M.color2 = 'assets/color2.jpg'
+M.color3 = 'assets/color3.jpg'
+M.color4 = 'assets/color4.jpg'
 -- Default background
-M.bg_image = M.bg_blurred
+M.bg_image = M.color2
 
 return M
