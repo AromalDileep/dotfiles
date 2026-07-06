af8259c29400a529ca80f06b3fbe892ffd344bc9
Aromal Dileep <aromaldileep96@gmail.com>
2026-07-07 00:22:10 +0530

    Confirmation added to nvim to ask yes or no before quitting NVIM

--- nvim/.config/nvim/lua/polish.lua
+++ nvim/.config/nvim/lua/polish.lua
@@ -52,3 +52,10 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 
 -- Apply once on startup
 make_transparent()
+
+-- Prevent accidentally quitting Neovim with <Leader>q by asking for confirmation.
+vim.keymap.set("n", "<Leader>q", function()
+  local choice = vim.fn.confirm("Quit Neovim?", "&Yes\n&No", 2)
+
+  if choice == 1 then vim.cmd "qa" end
+end, { desc = "Quit Neovim (confirm)" })
